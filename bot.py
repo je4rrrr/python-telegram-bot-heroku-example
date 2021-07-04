49b37d492c63bed87c1071924275760df576cb1f
je4rrrr <jerry.tan@u.nus.edu>
2021-07-05 02:06:50 +0800

    i'm a mess

--- bot.py
+++ bot.py
@@ -5,36 +5,33 @@ import requests
 from numpy import arctan
 from datetime import date
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
-mode = "dev"
 TOKEN = "1662384534:AAHRMfA1NZMIInG1pLtCKwAs4YG1JA4NQeM"
 HEROKU_APP_NAME = "tranquil-acadia-50401"
+PORT = int(os.environ.get('PORT', 8443))
 
-if mode == "dev":
-    def run(updater):
-        updater.start_polling()
-elif mode == "prod":
-    def run(updater):
-        PORT = int(os.environ.get("PORT", "8443"))
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-        updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN,
-                      webhook_url="https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
-        updater.idle()
-else:
-    logger.error("No MODE specified!")
-    sys.exit(1)
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-def start_handler(update, context):
-    stations = calculate_wbgt()
-    update.message.reply_text(stations)
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def calculate_wbgt():
     today = str(date.today())
@@ -77,10 +74,39 @@ def calculate_wbgt():
         
     return stations
 
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN,
+                      webhook_url="https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+
 if __name__ == '__main__':
-    logger.info("Starting bot")
-    updater = Updater(TOKEN)
-
-    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
-
-    run(updater)
+    main()
