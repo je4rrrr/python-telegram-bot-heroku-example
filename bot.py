0a21980bb0e51bb1ef256a160d2a4ac0166bc522
je4rrrr <jerry.tan@u.nus.edu>
2021-07-06 13:00:40 +0800

    bugfix token config var

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
-TOKEN = os.environ.get("TOKEN")
+TOKEN = os.environ['TOKEN']
 HEROKU_APP_NAME = "tranquil-acadia-50401"
 PORT = int(os.environ.get('PORT', 8443))
 
